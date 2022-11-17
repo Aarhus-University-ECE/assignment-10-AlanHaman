9887e9ec4ec356a3710bf1cbe3978173ec3903e8
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-11-17 13:34:07 +0100

    Update btree.c

--- src/btree.c
+++ src/btree.c
@@ -8,10 +8,6 @@
 struct tree_node* Insert(int x, struct tree_node* t) {
 	// Insert item x into the tree t
 
-	/*Base Case: When you reach a point where there is no node, either
-	because the tree is empty, or because there is a child to another
-	tree node to be inserted, a new node "temp" will be created, and
-	have its values filled out, to the be inserted*/
 	if (t == NULL) {
 		struct tree_node* temp = malloc(sizeof(struct tree_node));
 		temp->item = x;
@@ -21,10 +17,7 @@ struct tree_node* Insert(int x, struct tree_node* t) {
 		printf("%d\n", t->item);
 		return temp;
 	}
-	/*Special case: if the item to be inserted is equal to another item already
-	in the list, a "temp" node is created, for which to be inserted into the tree
-	it will be placed as the left child of the node that it shares a value with,
-	thus the logic of the tree prevails*/
+
 	if (x == t->item) {
 		struct tree_node* temp = malloc(sizeof(struct tree_node));
 		temp->item = x;
@@ -34,11 +27,6 @@ struct tree_node* Insert(int x, struct tree_node* t) {
 		return t;
 	}
 
-
-	/*Recursive step: if base case isn't true, then the algorithm
-	checks if the value to be inserted, is to be the right child or
-	the left child of the current node, and cals the function
-	for that nodes right or left child*/
 	if (x < t->item) {
 		t->left = Insert(x, t->left);
 		return t;
@@ -52,35 +40,23 @@ struct tree_node* Remove(int x, struct tree_node* t) {
 	// Remove one item from the tree t
 
 
-	// IF the tree is empty, there is nothing to remove
+
 	if (t == NULL) {
 		return t;
 	}
 
-	// If the number you want to remove doesn't exist, it will return the tree untouched
-	// Other wise it will run through the function
 	if (Contains(x, t) == 0) {
 		return t;
 	} else {
 
-		// Base Case: We reach the tree node that we want to remove
 		if (x == t->item) {
 
-			/*If the node to be removed has no children, a temporary node is made
-			that is equal to the right child, which is NULL in this case, so that
-			we can free the node, thus removing the value, and then return a NULL
-			node, to jump out of the function*/
 			if (t->left == NULL && t->right == NULL) {
 				struct tree_node* temp = t->right;
 				free(t);
 				return temp;
 			}
 
-			/*If only the right child exists, a temp node is made, that will
-			be equal to the right child of the node to be removed, so that
-			we can free the node to be removed, and then return the right child.
-			THis upholds the trees invariant, and through the recursion, it will
-			be linked to the previous node*/
 			if (t->left == NULL && t->right != NULL) {
 				struct tree_node* temp = t->right;
 				free(t);
@@ -92,13 +68,6 @@ struct tree_node* Remove(int x, struct tree_node* t) {
 				free(t);
 				return temp;
 			}
-			/*If both children exist, we make a temp node again, and make it equal
-			to the right child, we then make the temp go all the way to the right
-			childs left most child. This value we find will be larger than all the numbers
-			to the left of the node to be removed, but also smaller than all the numbers to
-			the right of the node to be removed, thus logic is upheld. We switch the two values
-			of the temp node and node to be removed, effectively removing the first node, thus
-			we free the temp node and return the new node value for the replacement*/
 			else if (t->left != NULL && t->right != NULL) {
 				struct tree_node* temp = t->right;
 				while (temp->left != NULL) {
@@ -110,8 +79,7 @@ struct tree_node* Remove(int x, struct tree_node* t) {
 				return t;
 			}
 		}
-		/*Same idea as for contains search, but recursive step
-		that will link together the tree, through removal process.*/
+		
 		else if (x < t->item) {
 			t->left = Remove(x, t->left);
 			return t;
@@ -124,11 +92,9 @@ struct tree_node* Remove(int x, struct tree_node* t) {
 
 int Contains(int x, struct tree_node* t) {
 
-	// Return true if the tree t contains item x. Return false otherwise.
+	
 
-
-	/*Runs through tree to see if the value we are searching for
-	exists in the tree, if yes then return 1, if no then return 0*/
+	
 	while (t != NULL) {
 		printf("%d -> ", t->item); // For aesthetics and testing
 		if (x == t->item) {
@@ -152,8 +118,6 @@ struct tree_node* Initialize(struct tree_node* t) {
 }
 
 int Empty(struct tree_node* t) {
-	// If there is no value for the root of the tree
-	// i.e. there is no tree, return true for empty
 	if (t == NULL) {
 		return 1;
 	}
@@ -161,7 +125,5 @@ int Empty(struct tree_node* t) {
 }
 
 int Full(struct tree_node* t) {
-	// Test if full
-	// Not applicable
 	return 0;
 }
